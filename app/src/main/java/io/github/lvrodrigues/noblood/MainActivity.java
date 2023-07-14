4359860ec4cc8f4de8fe2d12e46e42cfb717f314
LVRodrigues <lvrodriguesline@gmail.com>
2023-07-14 13:22:09 -0300

    Iniciando página principal.

--- app/src/main/java/io/github/lvrodrigues/noblood/MainActivity.java
+++ app/src/main/java/io/github/lvrodrigues/noblood/MainActivity.java
@@ -3,12 +3,28 @@ package io.github.lvrodrigues.noblood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton config;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        config = findViewById(R.id.config);
+        config.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Executar a Configuração", Snackbar.LENGTH_LONG)
+                        .setAction("Acttion", null)
+                        .show();
+            }
+        });
     }
 }
