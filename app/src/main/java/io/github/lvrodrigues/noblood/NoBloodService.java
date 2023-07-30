cfb5609bf4d8db3e365aadefbd8893a7d4aaeada
LVRodrigues <lvrodriguesline@gmail.com>
2023-07-29 22:59:04 -0300

    Notificando o serviço.

--- app/src/main/java/io/github/lvrodrigues/noblood/NoBloodService.java
+++ app/src/main/java/io/github/lvrodrigues/noblood/NoBloodService.java
@@ -42,6 +42,7 @@ public class NoBloodService extends Service {
                     .setContentIntent(pendingIntent)
                     .setTicker(getText(R.string.app_name))
                     .setVisibility(Notification.VISIBILITY_PUBLIC)
+                    .setShowWhen(false)
                     .setOngoing(true);
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 builder = builder.setForegroundServiceBehavior(Notification.FOREGROUND_SERVICE_IMMEDIATE);
